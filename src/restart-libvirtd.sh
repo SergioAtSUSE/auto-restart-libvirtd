e61a99dbae98e1d3183088572075f7735fc4f81b
Sergio Lindo <sergiolindo.empresa@gmail.com>
2019-08-28 16:02:46 +0200

    Remove temporary file to avoid race conditions
    - If libvirtd was restarted but no new job gets assigned to the SUT
    within the next hour, the
    file would still contain the string and the bash script will restart
    libvirtd again.

--- src/restart-libvirtd.sh
+++ src/restart-libvirtd.sh
@@ -2,4 +2,5 @@
 grep "qemu-system-s390x: cannot set up guest memory 's390.ram': Cannot allocate memory" /tmp/os-autoinst-*-stderr.log
 if [ "$?" -eq 0 ]; then
   systemctl restart libvirtd
+  rm /tmp/os-autoinst-*-stderr.log
 fi
